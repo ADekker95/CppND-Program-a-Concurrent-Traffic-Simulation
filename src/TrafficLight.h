c32e1f213fec24379502150e0cac45dea10cff05
ADekker95 <70537263+ADekker95@users.noreply.github.com>
2022-10-10 15:04:23 +0200

    Update TrafficLight.h

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -33,21 +33,26 @@ private:
 class TrafficLight
 {
 public:
-    // constructor / desctructor
+    // constructor / destructor
+    TrafficLight(); 
+    ~TrafficLight();
 
     // getters / setters
-
-    // typical behaviour methods
+    void waitForGreen();
+    void simulate();
+    TrafficLightPhase getCurrentPhase();
 
 private:
     // typical behaviour methods
-
+    void cycleThroughPhases();
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
 
     std::condition_variable _condition;
     std::mutex _mutex;
+	MessageQueue<TrafficLightPhase> _msgqueue; // why here TrafficLightPhase instead of T?  
+    TrafficLightPhase _currentPhase;
 };
 
 #endif

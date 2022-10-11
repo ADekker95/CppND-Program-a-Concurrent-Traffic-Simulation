54fd6db9f7075f9b9316e069e468852e3ff53a8b
ADekker95 <70537263+ADekker95@users.noreply.github.com>
2022-10-11 10:57:11 +0200

    Update TrafficLight.h

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -9,18 +9,24 @@
 // forward declarations to avoid include cycle
 class Vehicle;
 
-
+// DONE!!
 // FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
 // Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
 // Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
 // Also, there should be an std::condition_variable as well as an std::mutex as private members. 
 
+
 template <class T>
 class MessageQueue
 {
 public:
+	void send(T &&msg); 
+    T receive(); //Why here everything with t
 
 private:
+    std::deque<T> _queue; // queue stores objects of type TrafficLightPhase
+	std::condition_variable _condition_var;
+    std::mutex _mutex;
     
 };
 
@@ -29,8 +35,13 @@ private:
 // as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that 
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
+enum TrafficLightPhase 
+{
+	red,
+    green
+};
 
-class TrafficLight
+class TrafficLight : public TrafficObject
 {
 public:
     // constructor / destructor
@@ -40,19 +51,22 @@ public:
     // getters / setters
     void waitForGreen();
     void simulate();
+
+    // typical behaviour methods
     TrafficLightPhase getCurrentPhase();
 
 private:
     // typical behaviour methods
-    void cycleThroughPhases();
+	void cycleThroughPhases();
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
-
-    std::condition_variable _condition;
-    std::mutex _mutex;
 	MessageQueue<TrafficLightPhase> _msgqueue; // why here TrafficLightPhase instead of T?  
     TrafficLightPhase _currentPhase;
+    
+    std::condition_variable _condition;
+    std::mutex _mutex;
+    
 };
 
 #endif
